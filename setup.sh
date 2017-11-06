2b17a476ec562af010ab9ae7ac12c957a55b6513
mpavlov <fgvbrt123@gmail.com>
2017-11-06 14:38:24 +0300

    move setup scripts in root dir

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ conda create -n nips_rl_fast3 python=3.5.1 mkl-service -y
 source activate nips_rl_fast3
 conda install -c conda-forge lapack git -y
 pip install -r requirements.txt
-pip install -e ../opensim/python3.5/site-packages
+pip install -e opensim/python3.5/site-packages
